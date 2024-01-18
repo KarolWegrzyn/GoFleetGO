b7d57f925845be86341fb431b4cfddc953750234
Węgrzyn_Karol <rwegrzynkarol@gmail.com>
2024-01-18 18:45:01 +0100

    add ui

---
+++ src/main/java/UI/DataBaseController.java
@@ -0,0 +1,70 @@
+package UI;
+
+import org.example.gofleetgo.User;
+import org.example.gofleetgo.UserRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseController {
+    // Dane dostępowe do bazy danych
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/gofleetgo";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    // Metoda do nawiązywania połączenia
+    public static Connection connect() {
+        try {
+            // Rejestrowanie sterownika JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Nawiązywanie połączenia
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            System.out.println("Connected to the database");
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Metoda do zamykania połączenia
+    public static void disconnect(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Disconnected from the database");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //metoda do sprawdzania poprawnosci logowania
+    public boolean isDataLoginCorrect(String login, String password)
+    {
+
+
+        UserRepository sampleUser = new UserRepository();
+        User sampUser = sampleUser.findUserByUsername(login);
+
+        if(password == sampUser.getPassword())
+        {
+            return true;
+        }
+   
+
+
+
+
+        return true;
+    }
+
+    public Boolean correctLoginAndPasswordBasedonLogin(String login)
+    {
+
+        return true;
+    }
+
+}
---
+++ src/main/java/UI/Main.java
@@ -0,0 +1,34 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import static UI.DataBaseController.connect;
+import static UI.DataBaseController.disconnect;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setTitle("GoFleetGo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        DataBaseController controller = new DataBaseController();
+        Connection connection = connect();
+        //System.out.println(controller.isDataLoginCorrect("root","1111"));
+        System.out.println(connection);
+        launch();
+        disconnect(connection);
+    }
+}
